2b2510eb34a8152e4c8681489014c93cec70858d
Soham Nimbalkar <sohamnimbalkar07@gmail.com>
2025-12-19 01:05:02 +0530

    adding singleton pattern

--- singleton-pattern/java/SingletonClass.java
+++ singleton-pattern/java/SingletonClass.java
@@ -11,6 +11,7 @@ public class SingletonClass {
             synchronized (SingletonClass.class) {
                 if (instance == null) {
                     instance = new SingletonClass();
+
                 }
             }
         }
